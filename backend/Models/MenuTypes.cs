8ee6b5ef02fcd12c33701919c0ee9043c459050e
Alexander Sommer <Alexander.Sommer@akqa.com>
2023-10-18 14:50:13 +0200

    Rough implementation of "GetMenuTypesById".

---
+++ backend/Models/MenuTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Models
+{
+    public class MenuTypes
+    {
+        public int MenuTypeId { get; set; }
+        public string? Name { get; set; }
+        public string? DefaultStartTime { get; set; }
+        public string? DefaultEndTime { get; set; }
+    }
+}
