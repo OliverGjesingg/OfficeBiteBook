331ce35b1fd7e96f13ea5d31ef7d4a3ddfc44510
Alexander Sommer <Alexander.Sommer@akqa.com>
2023-11-08 13:58:29 +0100

    Created new frontend's - manual env.local files & keys required to establish connection with the database (Supabase)

---
+++ frontend/frontend/app/menus/page.tsx
@@ -0,0 +1,62 @@
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+export default async function Menu() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const { data: menus } = await supabase.from("menu").select();
+
+  return (
+    <>
+      {menus.map((menu) => (
+        <section key={menu.menu_id}>
+          <ul>
+            <li>
+              <p>
+                <b>Menu ID</b>
+              </p>
+              <p>{menu.menu_id}</p>
+            </li>
+            <li>
+              <p>
+                <b>Title</b>
+              </p>
+              <p>{menu.title}</p>
+            </li>
+            <li>
+              <p>
+                <b>Start Date</b>
+              </p>
+              <p>{menu.start_date}</p>
+            </li>
+            <li>
+              <p>
+                <b>End Date</b>
+              </p>
+              <p>{menu.end_date}</p>
+            </li>
+            <li>
+              <p>
+                <b>Menu Type ID</b>
+              </p>
+              <p>{menu.menu_type_id}</p>
+            </li>
+            <li>
+              <p>
+                <b>Room ID</b>
+              </p>
+              <p>{menu.room_id}</p>
+            </li>
+            <li>
+              <p>
+                <b>Published?</b>
+              </p>
+              <p>{JSON.stringify(menu.published)}</p>
+            </li>
+          </ul>
+          <br />
+        </section>
+      ))}
+    </>
+  );
+}
